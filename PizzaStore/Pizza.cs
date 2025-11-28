b2c33dc07244844c69f87afbcb8d35cb0b43ad12
lidiaAsamnew <lidia.yirbe@gmail.com>
2025-11-28 19:13:06 +0300

    Define Pizza model

--- PizzaStore/Pizza.cs
+++ PizzaStore/Pizza.cs
@@ -1,1 +1,9 @@
-//pizza.cs
+using Microsoft.EntityFrameworkCore;
+using PizzaStore.Models;
+
+public class PizzaDb : DbContext
+{
+    public PizzaDb(DbContextOptions<PizzaDb> options) : base(options) { }
+
+    public DbSet<Pizza> Pizzas => Set<Pizza>();
+}
